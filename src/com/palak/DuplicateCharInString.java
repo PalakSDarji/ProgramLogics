cf1585b8bdb027767c8d7ba788f12e67a68afa48
PalakSDarji <palaksdarji@gmail.com>
2022-06-23 13:04:44 +0530

    Duplicate chars in string example

---
+++ src/com/palak/DuplicateCharInString.java
@@ -0,0 +1,28 @@
+package com.palak;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DuplicateCharInString {
+
+    private Map<Character,Integer> map = new HashMap<>();
+
+    private void checkDuplicateCharsInStringAndPrint(String input){
+
+        for(int i =0; i<input.length(); i++){
+            char c = input.charAt(i);
+            int count = 0;
+            if(map.containsKey(c)){
+                count = map.get(c);
+            }
+            map.put(c,++count);
+        }
+
+        System.out.println(map);
+    }
+
+    public static void main(String[] args) {
+
+        new DuplicateCharInString().checkDuplicateCharsInStringAndPrint("Java Palak is love");
+    }
+}
