5d08a01d53af3f439a0bfbd28ec547f26d5d0fa8
PalakSDarji <palaksdarji@gmail.com>
2022-06-23 18:01:10 +0530

    String reversal Array of Generics Reversal in-place example

--- src/com/palak/ArrayReversalWithoutBuffer.java
+++ src/com/palak/ArrayReversalWithoutBuffer.java
@@ -7,10 +7,6 @@ public class ArrayReversalWithoutBuffer {
     private int[] reverseArray(int[] arr){
         for(int i=0;i<arr.length/2;i++) {
 
-            if(i == arr.length-1){
-                break;
-            }
-
             int firstElement = arr[i];
             int lastElement = arr[arr.length-1-i];
 
@@ -21,8 +17,27 @@ public class ArrayReversalWithoutBuffer {
         return arr;
     }
 
+    private <T> T[] reverseGenericArr(T[] arr){
+        for(int i=0;i<arr.length/2;i++){
+
+            T temp = arr[i];
+            T replacement = arr[arr.length-1-i];
+
+            arr[i] = replacement;
+            arr[arr.length-1-i] = temp;
+        }
+
+        return arr;
+    }
+
+    private void reverseString(String input){
+        System.out.println(Arrays.toString(reverseGenericArr(input.split(" "))));
+    }
+
     public static void main(String[] args) {
         ArrayReversalWithoutBuffer arrayReversalWithoutBuffer = new ArrayReversalWithoutBuffer();
         System.out.println(Arrays.toString(arrayReversalWithoutBuffer.reverseArray(new int[]{2,3,6})));
+
+        arrayReversalWithoutBuffer.reverseString("Java is love for life");
     }
 }
