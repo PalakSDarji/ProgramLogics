09ca06c787a0d689490236f54e5b49645c8fd508
PalakSDarji <palaksdarji@gmail.com>
2022-07-15 23:58:29 +0530

    Simple memory leak in java using 3 lines.

---
+++ src/com/palak/java/MemoryLeakPlay.java
@@ -0,0 +1,13 @@
+package com.palak.java;
+
+import java.util.Vector;
+
+public class MemoryLeakPlay {
+    public static void main(String[] args) {
+        Vector v = new Vector(2144444444);
+        Vector v1 = new Vector(214744444);
+        Vector v2 = new Vector(214444444);
+        System.out.println("Memory Leaks Example");
+        //Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+    }
+}
