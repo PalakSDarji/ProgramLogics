b23c9512cc3f280188d2ce536ddca95a2fe419f9
PalakSDarji <palaksdarji@gmail.com>
2022-07-27 10:45:34 +0530

    Selection sort

---
+++ src/com/palak/sorting/SelectionSort.java
@@ -0,0 +1,39 @@
+package com.palak.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    /**
+     * 5,3,4,1
+     * 1,3,4,5
+     */
+    private void sort(int[] arr){
+
+        int currentSortedIndex = 0;
+        for(int i=0; i< arr.length; i++){
+            int min = arr[currentSortedIndex];
+            int minIndex = currentSortedIndex;
+            for(int j=currentSortedIndex; j< arr.length; j++){
+                if(arr[j] < min){
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            if(arr[i] > min){
+                int temp = arr[i];
+                arr[i] = min;
+                arr[minIndex] = temp;
+            }
+            currentSortedIndex++;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{5,3,4,1};
+        new SelectionSort().sort(arr);
+    }
+}
