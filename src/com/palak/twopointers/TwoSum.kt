1f9378ccf6693694f59c3514505e00f9680323e3
Palakkumar Darji <palaksdarji@gmail.com>
2025-11-02 09:07:07 +0530

    updated to latest

--- src/com/palak/twopointers/TwoSum.kt
+++ src/com/palak/twopointers/TwoSum.kt
@@ -1,6 +1,53 @@
 package com.palak.twopointers
 
-class PairSum {
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+*/
+
+fun main() {
+    val nums1 = intArrayOf(2,7,11,15)
+    val nums2 = intArrayOf(3,2,4)
+    val nums3 = intArrayOf(3,3)
+
+    val ps = TwoSum()
+
+    ps.twoSum(nums1, 9).print()
+    ps.twoSumBruteForce(nums1, 9).print()
+    ps.twoSumHashMap(nums1, 9).print()
+
+    ps.twoSum(nums2, 6).print()
+    ps.twoSumBruteForce(nums2, 6).print()
+    ps.twoSumHashMap(nums2, 6).print()
+
+    ps.twoSum(nums3, 6).print()
+    ps.twoSumBruteForce(nums3, 6).print()
+    ps.twoSumHashMap(nums3, 6).print()
+}
+
+public fun IntArray.print() {
+    this.forEach(::println)
+    println("-----")
+}
+
+class TwoSum {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val resultArray = IntArray(2)
         nums.sort()
@@ -15,6 +62,7 @@ class PairSum {
             } else {
                 resultArray[0] = left
                 resultArray[1] = right
+                return resultArray
             }
         }
 
